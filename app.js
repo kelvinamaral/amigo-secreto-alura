d9d3cf7b79beb461642f7216eefb2bcee704466b
Kelvin <kelvinfamaral@hotmail.com>
2025-02-05 14:08:14 -0300

    Implementa uma função para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(nome);
+        console.log(amigos);
+        //limpa o input
+        document.getElementById('amigo').value = "";
+        //redefine o campo de entrada para uma string vazia
+        document.getElementById('amigo').placeholder = "";
+    }
+}
